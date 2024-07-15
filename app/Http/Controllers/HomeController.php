377296f0b96dfc47b503b72f323f9f3b3b0def71
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-07-15 20:00:08 +0600

    including-bootstrap-asssets

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,13 +6,7 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    function Home(Request $request) {
-        $data = [
-            ['name' => 'Pritam', 'city' => 'Dhaka'],
-            ['name' => 'Priya', 'city' => 'Jessore'],
-            ['name' => 'Prity', 'city' => 'Jhenaidah'],
-            ['name' => 'Pori', 'city' => 'Khulna'],
-        ];
-        return view('Home', ['data' => $data]);
+    function Home() {
+        return view('Home');
     }
 }
--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -4,13 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}" type="text/css">
     <title>Laravel Blade Template</title>
 </head>
 <body>
-    <ul>
-        @foreach ($data as $eachData)
-            <li> User Name is : {{ $eachData['name'] }} and City is : {{ $eachData['city'] }} </li>
-        @endforeach
-    </ul>
+    <div class="container mt-5">
+        <div class="row d-flex justify-content-center">
+            <div class="col-6">
+                <button class="btn btn-success">Button</button>
+                <img class="w-50" src={{ asset('images/laravel.png') }} alt="Laravel" />
+            </div>
+        </div>
+    </div>
 </body>
+<script src="{{ asset('js/bootstrap.bundle.js') }}"></script>
 </html>
